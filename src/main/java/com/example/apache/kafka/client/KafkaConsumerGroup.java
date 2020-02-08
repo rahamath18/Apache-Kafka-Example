a0a27a6d0a33a9d181a4c128967ddce5b0a0a2e8
Rahamathullah <rahamath18@yahoo.com>
2020-02-08 13:28:27 +0530

    updated

--- src/main/java/com/example/apache/kafka/client/KafkaConsumerGroup.java
+++ src/main/java/com/example/apache/kafka/client/KafkaConsumerGroup.java
@@ -18,8 +18,6 @@ public class KafkaConsumerGroup {
 		props.put("enable.auto.commit", "true");
 		props.put("auto.commit.interval.ms", "1000");
 		props.put("session.timeout.ms", "30000");
-		//props.put("key.deserializer", "org.apache.kafka.common.serialization.ByteArraySerializer");
-		//props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 	    props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
